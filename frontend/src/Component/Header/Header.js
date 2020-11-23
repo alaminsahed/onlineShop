1f752f37b6900e9a471e10111930ee5dcc9c206f
Al-amin Sahed <alaminsahed97@gmail.com>
2020-11-23 21:47:25 +0600

    product-details done

--- frontend/src/Component/Header/Header.js
+++ frontend/src/Component/Header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import {LinkContainer} from "react-router-bootstrap";
 const Header = () => {
   return (
     <header>
@@ -10,8 +11,12 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <NavLink to="/cart"><i className="fas fa-shopping-cart"></i>Cart</NavLink>
-            <NavLink to="/login"><i className="fas fa-user"></i>Login</NavLink>
+            <LinkContainer to="/cart">
+            <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+            <Nav.Link ><i className="fas fa-user"></i>Login</Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
         </Container>
--- frontend/src/HomePage/ProductDetails.js
+++ frontend/src/HomePage/ProductDetails.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import data from '../data';
+import React from "react";
+import { Button, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import Rating from "../Component/Rating/Rating";
+import data from "../data";
 
 const ProductDetails = () => {
-    const {id} = useParams();
-    const product = data.find((p)=>p._id=== id)
-    return (
-        <div>
-          <h1>{product.name}</h1>
-        </div>
-    );
+  const { id } = useParams();
+  const product = data.find((p) => p._id === id);
+  return (
+    <div>
+      <Link to="/">Go Back</Link>
+
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={6}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>Description: {product.description}</p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+            <h5>Price: ${product.price}</h5>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <p>Available: {product.countInStock}</p>
+              {product.countInStock<=0? <p style={{color:'red'}}>Out Of Stock</p>:<p style={{color:'green'}}>In Stock</p>}
+            </ListGroup.Item>
+            <ListGroup.Item>
+             <Button className="btn-block cart-button" type="button" disabled={product.countInStock===0}>Add To Cart</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default ProductDetails;
