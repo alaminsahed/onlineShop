c3af48df830c3f52414992062e1610722d852be8
Al-amin Sahed <alaminsahed97@gmail.com>
2021-01-23 12:48:39 +0600

    git problem

--- backend/Models/userModel.js
+++ backend/Models/userModel.js
@@ -30,6 +30,22 @@ userSchema.methods.matchPassword = async function (enteredPassword){
     return await bcrypt.compare(enteredPassword, this.password);
 }
 
+<<<<<<< Updated upstream
+=======
+// hasing the password using middle ware in schema
+
+userSchema.pre('save', async function(next)
+{
+    if(!this.isModified('password')){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+}
+)
+
+>>>>>>> Stashed changes
 const User = mongoose.model('User', userSchema);
 
 export default User;
