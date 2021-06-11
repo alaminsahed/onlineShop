8eae0a8d7734063f00e5ac3d9544ffadbcbaa19d
Al-amin Sahed <alaminsahed97@gmail.com>
2021-06-11 20:21:34 +0600

    test-deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './store'
+import store from '../src/store'
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
